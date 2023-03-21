757964297fa8b85a8f134a226410d3fdffb0fd4f
Frederik0111 <fredez0111@gmail.com>
2023-03-21 17:05:38 +0100

    working

--- app.js
+++ app.js
@@ -26,4 +26,66 @@ function initApp() {
         potterActor,
         potterImage
     );
+
+    showCharacter(
+      potterImage,
+      potterName,
+      potterHouse,
+      potterDateOfBirth,
+      potterActor
+    );
+    // showCharacter(
+    //   hermioneImage,
+    //   hermioneName,
+    //   hermioneHouse,
+    //   hermioneOfBirth,
+    //   hermioneActor
+    // );
 }
+
+// const characters = document.querySelector("#characters");
+
+function showCharacter(image, name, house, dateOfBirth, actor) {
+    // visning af character elements  
+    const articleElement = document.createElement("article");
+    const imageElement = document.createElement("img");
+    const nameElement = document.createElement("h2");
+    const houseElement = document.createElement("p");
+    const dateOfBirthElement = document.createElement("p");
+    const actorElement = document.createElement("p");
+
+    nameElement.textContent = name;
+    imageElement.src = "http://hp-api.herokuapp.com/images/harry.jpg";
+    houseElement.textContent = house;
+    dateOfBirthElement.textContent = dateOfBirth;
+    actorElement.textContent = actor;
+
+    // dateOfBirthElement.textContent = `Date of Birth: ${dateOfBirth}`;
+    // actorElement.textContent = `${name} is played by ${actor}`;
+
+    articleElement.append(
+      imageElement,
+      nameElement,
+      houseElement,
+      dateOfBirthElement,
+      actorElement
+    );
+
+        document.querySelector("#characters").appendChild(articleElement);
+
+    console.log(
+        image,
+        name,
+        house,
+        dateOfBirth,
+        actor
+    );
+
+    console.log(
+        imageElement,
+        nameElement
+    );
+
+
+}
+
